c7384f0a645e1075d8e3d48e8a417b69e06544c6
ryu <wocks1123@gmail.com>
2020-03-20 13:26:27 +0900

    예제 코드 작성
    - 실행할 예제 코드 작성
    - 코드 빌드를 위한 CMakeLists.txt를 Sources/Proejct1에 추가

---
+++ Includes/Project1/Test.hpp
@@ -0,0 +1,10 @@
+//
+// Created by User on 2020-03-20.
+//
+
+#ifndef CPP_OSP_TEST_TEST_HPP
+#define CPP_OSP_TEST_TEST_HPP
+
+int Add(int a, int b);
+
+#endif //CPP_OSP_TEST_TEST_HPP
---
+++ Sources/Project1/Test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by User on 2020-03-20.
+//
+
+#include <Project1/Test.hpp>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
